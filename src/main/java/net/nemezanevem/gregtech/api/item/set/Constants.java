292a832e08562b7bbe1039f48090dd140d282a12
joona.stenberg <joona.v.stenberg@gmail.com>
2023-01-19 11:31:58 +0200

    beginnings

---
+++ src/main/java/net/nemezanevem/gregtech/api/item/set/Constants.java
@@ -0,0 +1,8 @@
+package net.nemezanevem.gregtech.api.item.set;
+
+public class Constants {
+    public static final String INGOT_KEY = "_ingot";
+    public static final String DUST_KEY = "_dust";
+    public static final String ORE_KEY = "_dust";
+    public static final String PLATE_KEY = "_plate";
+}
---
+++ src/main/java/net/nemezanevem/gregtech/api/registry/item/set/IItemSet.java
@@ -0,0 +1,10 @@
+package net.nemezanevem.gregtech.api.registry.item.set;
+
+public interface IItemSet {
+    boolean hasIngot();
+    boolean hasPlate();
+    boolean hasDust();
+    boolean hasFluid();
+    boolean hasOre();
+
+}
