cb77d0ed21151282149ac79c90134d44a106916f
NemEzanevem <99277804+NemEzanevem@users.noreply.github.com>
2023-01-19 17:48:26 +0100

    added stuff etc

--- src/main/java/net/nemezanevem/gregtech/api/recipe/ModRecipes.java
+++ src/main/java/net/nemezanevem/gregtech/api/recipe/ModRecipes.java
@@ -14,14 +14,96 @@ public class ModRecipes {
     public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(ForgeRegistries.RECIPE_TYPES, GregTech.MODID);
 
 
-
-
     public void registerRecipeTypes(IEventBus bus) {
         RECIPE_TYPES.register(bus);
         RECIPE_SERIALIZERS.register(bus);
         for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
             RECIPE_TYPES.register(machine.name, () -> new SimpleMachineRecipe(machine));
             RECIPE_SERIALIZERS.register(machine.name, () -> new SimpleMachineRecipeSerializer(machine));
+        }
+    }
+
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new EBFRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new EBFRecipeSerializer(machine));
+
+
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new BBFRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new BBFRecipeSerializer(machine));
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new FreezingRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new FreezingRecipeSerializer(machine));
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new PyroRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new PyroRecipeSerializer(machine));
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new LCRRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new LCRRecipeSerializer(machine));
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new DistillingRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new DistillingRecipeSerializer(machine));
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new Implosion_CompressorRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new Implosion_CompressorRecipeSerializer(machine));
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new Oil_Cracking_UnitRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new Oil_Cracking_UnitRecipeSerializer(machine));
+        }
+    }
+
+    public void registerRecipeTypes(IEventBus bus) {
+        RECIPE_TYPES.register(bus);
+        RECIPE_SERIALIZERS.register(bus);
+        for (Machine machine : MachineRegistry.MACHINES_BUILTIN.get()) {
+            RECIPE_TYPES.register(machine.name, () -> new Coke_OvenRecipe(machine));
+            RECIPE_SERIALIZERS.register(machine.name, () -> new Coke_OvenRecipeSerializer(machine));
         }
     }
 }
